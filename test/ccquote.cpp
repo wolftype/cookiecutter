e6eec66333463ad2727e350f7d8c7dac85b34e65
wolftype <wolftype@gmail.com>
2016-04-04 16:18:01 -0700

    ccquote test and updated readme

---
+++ test/ccquote.cpp
@@ -0,0 +1,48 @@
+/// If called with a file/*.json, will load data and analyze cost
+/// Otherwise, prints cost of
+/// Rectangle.json, ExtrudeCircularArc.json, and CutCircularArc.json
+
+#include "cc.hpp"
+
+#include <iostream> //std::cout
+#include <iomanip>  //std::setprecision
+
+using namespace cc;
+using namespace std;
+
+/// Pretty Printing
+void print(double cost){
+  int prec = Cost::Decimals(cost,2);
+  cout << "Estimated Cost: $" <<  std::setprecision(prec) << cost << " US Dollars."<< endl;
+}
+
+/// Pass optional "files/*.json" argument to binary
+int main(int argc, char * argv[]) {
+
+  Data data;
+  double cost;
+  int resolution = 20;
+
+  /// process argument if one exists, otherwise process 3 default files
+  if (argc > 1) {
+    data.load(argv[1]);
+    cost = data.cost(resolution);
+    print(cost);
+  } else {
+    data.load("files/Rectangle.json");
+    cost = data.cost(resolution);
+    print(cost);
+
+    data.load("files/ExtrudeCircularArc.json");
+    cost = data.cost(resolution);
+    print(cost);
+
+    data.load("files/CutCircularArc.json");
+    cost = data.cost(resolution);
+    print(cost);
+
+  }
+
+  return 0;
+
+}
